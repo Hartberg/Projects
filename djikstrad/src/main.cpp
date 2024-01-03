d3e2b678123a5fb444521caa2eaee6763d40dfad
hartberg <jonashartberg@gmail.com>
2024-01-03 20:12:27 +0100

    har global vei

--- djikstrad/src/main.cpp
+++ djikstrad/src/main.cpp
@@ -12,119 +12,29 @@
 
 int currentVerticy; // nåværende plassering
 int nodeList[V];  // liste med punkter til mål
-bool whatnodes[V];  // liste med alle noder i shortest path som 
+int shortestPath[V];  // liste med alle noder i shortest path som 
+int nrTurn = 1; // burkes til å iterere hvor man putter inn nodene i shortestPath[]
 
 Zumo32U4ButtonA buttonA;
 
-int graph[V][V] = {{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-                     {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0},
-                     {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1},
-                     {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
-                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
-                     {0, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                     {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0}};
+int graph[V][V] = {{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                   {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                   {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                   {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                   {0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                   {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                   {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0}, 
+                   {0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, 
+                   {0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
+                   {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0}, 
+                   {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0}, 
+                   {0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0}, 
+                   {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+                   {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, 
+                   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1}, 
+                   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
+                   {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}};
 
-// A utility function to find the vertex with minimum
-// distance value, from the set of vertices not yet included
-// in shortest path tree
-int minDistance(int dist[], bool sptSet[])
-{
-
-  // Initialize min value
-  int min = 10000, min_index;
-
-  for (int v = 0; v < V; v++)
-    if (sptSet[v] == false && dist[v] <= min)
-      min = dist[v], min_index = v;
-
-  return min_index;
-}
-
-// A utility function to print the constructed distance
-// array
-void printSolution(int dist[])
-{
-  Serial.println("this is the shortest path ");
-  for (int i = 0; i < V; i++) {
-    Serial.print(i);
-    Serial.print("    ");
-    Serial.print(dist[i]);
-    Serial.println(" ");
-  }
-}
-
-// Function that implements Dijkstra's single source
-// shortest path algorithm for a graph represented using
-// adjacency matrix representation
-void dijkstra(int graph[V][V], int src)
-{
-  int dist[V]; // The output array. dist[i] will hold the
-               // shortest
-  // distance from src to i
-
-  bool sptSet[V]; // sptSet[i] will be true if vertex i is
-                  // included in shortest
-  // path tree or shortest distance from src to i is
-  // finalized
-
-  // Initialize all distances as INFINITE and stpSet[] as
-  // false
-  for (int i = 0; i < V; i++)
-    dist[i] = 10000, sptSet[i] = false;
-
-  // Distance of source vertex from itself is always 0
-  dist[src] = 0;
-
-  // Find shortest path for all vertices
-  for (int count = 0; count < V - 1; count++)
-  {
-    // Pick the minimum distance vertex from the set of
-    // vertices not yet processed. u is always equal to
-    // src in the first iteration.
-    int u = minDistance(dist, sptSet);
-
-    // Mark the picked vertex as processed
-    sptSet[u] = true;
-
-    // Update dist value of the adjacent vertices of the
-    // picked vertex.
-    for (int v = 0; v < V; v++)
-
-      // Update dist[v] only if is not in sptSet,
-      // there is an edge from u to v, and total
-      // weight of path from src to v through u is
-      // smaller than current value of dist[v]
-      if (!sptSet[v] && graph[u][v] && dist[u] != 100000 && dist[u] + graph[u][v] < dist[v])
-        dist[v] = dist[u] + graph[u][v];
-  }
-  
-  // print the constructed distance array
-  printSolution(dist);
-}
-
-// driver's code
-int mains()
-{
-
-  /* Let us create the example graph discussed above */
-  
-
-  // Function call
-  dijkstra(graph, 5);
-  
-
-  return 0;
-}
 
 // Define a structure to store node information including parent
 struct NodeInfo {
@@ -136,12 +46,17 @@ struct NodeInfo {
 void printPath(NodeInfo nodeInfo[], int j) {
   if (nodeInfo[j].parent == -1) {
     Serial.print(j);
+    shortestPath[nrTurn] = j;
+    nrTurn = 1; // resetter
     return;
   }
 
   printPath(nodeInfo, nodeInfo[j].parent);
   Serial.print(" -> ");
   Serial.print(j);
+  
+  shortestPath[nrTurn] = j; // legger til korteste vei i global array
+  nrTurn ++;
 }
 
 
@@ -149,6 +64,7 @@ void printPath(NodeInfo nodeInfo[], int j) {
 void dijkstraWithPath(int graph[V][V], int src, int dest) {
   int dist[V];     // Output array. dist[i] will hold the shortest distance from src to i
   bool sptSet[V];  // Will be true if vertex i is included in the shortest path tree
+  shortestPath[0] = src; // setter startpunkt i globalarray 
 
   NodeInfo nodeInfo[V]; // Information about each node (distance and parent)
   
@@ -195,6 +111,9 @@ void dijkstraWithPath(int graph[V][V], int src, int dest) {
   Serial.print(dest);
   Serial.print(": ");
   printPath(nodeInfo, dest);
+  for (int i = 0; i < V; i++) {
+    Serial.println(shortestPath[i]);
+  }
 }
 
 
@@ -234,23 +153,23 @@ void loop()
 {3, 4, 4, 1, 2, 3, 2, 3, 4, 2, 3, 5, 3, 4, 5, 0, 1},
 {4, 5, 5, 2, 3, 4, 3, 3, 5, 1, 2, 5, 2, 3, 4, 1, 0}}
 
+kjekkerstue
+{{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+{1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+{0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0}, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0}, 
+{0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0}, 
+{0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1}, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}}
 
-{adjecancy
-{{0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
-{1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
-{0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
-{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,}
-{1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
-{0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,}
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0,}
-{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0,}
-{0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1,}
-{0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,}
-{0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0,}
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0,}
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,}
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0,}
-{0, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,}
-{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0,}}
 */
