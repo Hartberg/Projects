a34744e0661c2b9a39b51549048702e2af8161a5
hartberg <jonashartberg@gmail.com>
2024-01-04 17:46:24 +0100

    med adj list

--- djikstrad/src/main.cpp
+++ djikstrad/src/main.cpp
@@ -11,20 +11,25 @@
 #define V 17
 
 // kjørevariabler
-int16_t position;                // bilens position ifrhold til linja 0-4000
-float lineMultiplier = 0;        // tallet hjulene skal ganges med for linjefølging
+int position;                // bilens position ifrhold til linja 0-4000
 unsigned int lineSensorArray[5]; // lager et array for bilen å sette tall inn i
-int normalSpeed = 150;           // basis fart for
+int normalSpeed = 200;           // basis fart for
+
+float lineMultiplier;
 
 int currentVerticy;  // nåværende plassering
 int nodeList[V];     // liste med punkter til mål
 int shortestPath[V]; // liste med alle noder i shortest path som
 int nrTurn = 1;      // burkes til å iterere hvor man putter inn nodene i shortestPath[]
 
+unsigned long millisTimer; // timer til oled skjerm
+unsigned long nodeDetectionCD; // samme som over
+
 Zumo32U4Motors motors;
 Zumo32U4ButtonA buttonA;
 Zumo32U4OLED oled;
 Zumo32U4LineSensors lineSensors;
+Zumo32U4Buzzer buzzer;
 
 int graph[V][V] = {{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
                    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -140,13 +145,14 @@ void calibrate()
   oled.gotoXY(0, 0);
   oled.print("calibrating");
   delay(500);
-  for (int i = 0; i < 250; i++)
+  for (int i = 0; i < 200; i++)
   {
     motors.setSpeeds(180, -180); // kjør i sirkel
     lineSensors.calibrate();
   }
   motors.setSpeeds(0, 0);
   oled.clear();
+  oled.setLayout21x8();
   delay(50); // tid til motoren og stoppe helt før den starter å kjøre fremover
 }
 
@@ -162,24 +168,61 @@ void lineFollowP()
   int speedLeft = normalSpeed * (1.0 - lineMultiplier);
   int speedRight = normalSpeed * (1.0 + lineMultiplier);
   motors.setSpeeds(speedLeft, speedRight);
-  oled.clear();
-  oled.gotoXY(0,0);
-  oled.println(speedLeft);
-  oled.gotoXY(0,1);
-  oled.println(speedRight);
+  //oled.clear();
+  //oled.gotoXY(0, 0);
+  //oled.println(speedLeft);
+  //oled.gotoXY(0, 1);
+  //oled.println(speedRight);
+}
+
+void printSensor()
+{
+  if (millis() - millisTimer > 20)
+  {
+    oled.clear();
+    oled.gotoXY(0, 0);
+    oled.print(lineSensorArray[0]);
+    oled.gotoXY(0,1);
+    oled.print(lineSensorArray[1]);
+    oled.gotoXY(0,2);
+    oled.print(lineSensorArray[2]);
+    oled.gotoXY(0,3);
+    oled.print(lineSensorArray[3]);
+    oled.gotoXY(0,4);
+    oled.print(lineSensorArray[4]);
+    oled.gotoXY(10,0);
+    oled.print(position);
+
+    millisTimer = millis();
+  }
+}
+
+bool nodeDetect(){
+  if ((lineSensorArray[1] + lineSensorArray[2] + lineSensorArray[3] > 1800) && (millis() - nodeDetectionCD > 500)) { 
+    buzzer.playFrequency(440, 100, 10);
+    
+    return true;
+  }
+  return false;
 }
 
 void setup()
 {
   Serial.begin(9600);
-  lineSensors.initFiveSensors(); // initier linjesensorer
+  lineSensors.initFiveSensors(); 
   calibrate();
- 
 }
 
 void loop()
 {
   readSensors();
+  printSensor();
+  if (nodeDetect()){
+    delay(100);
+    motors.setSpeeds(0,0);
+    delay(500);
+    nodeDetectionCD = millis();
+  }
   lineFollowP();
   /*
   if (buttonA.getSingleDebouncedPress())
@@ -228,4 +271,24 @@ kjekkerstue
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
 {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}}
 
+
+adjency list // nabo starter kl12 og følger klokken
+0 {{3,1},
+1  {2,0},
+2  {1,4},
+3  {6,0},
+4  {7,8,2},
+5  {8},
+6  {7,3,12},
+7  {4,6,9},
+8  {5,4},
+9  {10,7,13},
+10 {14,9},
+11 {15,8,14},
+12 {6},
+13 {9},
+14 {11,10,16},
+15 {11},
+16 {14}}
+ 
 */
