4ef35ef61b4de40bf86569d05ccea826d54e83f8
hartberg <jonashartberg@gmail.com>
2024-02-07 16:46:46 +0100

    yes

--- iels2021forsterker/src/main.cpp
+++ iels2021forsterker/src/main.cpp
@@ -1,19 +1,27 @@
 #include <Arduino.h>
+
 int potPin = 32;
-int VoutPin = 25;
+int dacPin = 25;
+int adcPin = 34;
 float potReading; // avlesning pot
+float adcReading; 
+float amp = 1; // amplifikering
 
 void readPot(int pin)
 {
   potReading = analogRead(pin);
 }
 
+
+
 void setup()
 {
+  Serial.begin(9600);
 }
 
 void loop()
 {
   readPot(potPin);
-  Serial.println(potReading);
+  adcReading = analogRead(adcPin);
+  dacWrite(dacPin, adcReading);
 }
